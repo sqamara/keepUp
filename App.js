1232077b9aaa4322c2865000a8da9033c77c9154
Sebastian Amara <sqamara@ucsd.edu>
2019-06-27 00:20:09 -0700

    added searchable contacts

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import SearchableFlatList from './src/SearchableList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+            <SearchableFlatList />
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ src/SearchableList.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
+import * as Contacts from 'expo-contacts';
+
+class FlatListDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+    };
+
+    this.arrayholder = [];
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  async makeRemoteRequest() {
+    this.setState({ loading: true });
+    const { data } = await Contacts.getContactsAsync({
+      fields: [Contacts.Fields.PhoneNumbers, Contacts.Fields.Image],
+    });
+
+    filteredData = data.filter((item) => {
+      if ('phoneNumbers' in item 
+      && 'name' in item 
+      && item != null 
+      && item != undefined)
+        return true; 
+      else 
+        return false; });
+
+    console.log(filteredData[0])
+
+
+    this.setState({
+      data: filteredData,
+      error: null,
+      loading: false,
+    });
+    this.arrayholder = filteredData;
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
+
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
+
+    const newData = this.arrayholder.filter((item) => {
+      const itemData = item.name.toUpperCase();
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.state.data.sort((a,b) => {return a.name.localeCompare(b.name)})}
+          renderItem={({ item }) => (
+            <ListItem
+              leftAvatar={{source: (item.imageAvailable? {uri: item.image.uri} : undefined)}}
+              title={item.name}
+            />
+          )}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </View>
+    );
+  }
+}
+
+export default FlatListDemo;
